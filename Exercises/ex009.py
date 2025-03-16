18aaaaf3d016c45941003435b11c772fb7eae610
MahmudBekkar <mahmud.bekkar99@gmail.com>
2025-03-16 15:38:39 +0300

    Update ex009.py

--- Exercises/ex009.py
+++ Exercises/ex009.py
@@ -1,2 +1,12 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 220543602
+# Your Name and Surname: Mahmud Bekkar
+X = input("Enter conversion type (C to F or F to C): ").strip().lower()
+temperature = float(input("Enter the temperature value: "))
+if X == "1":
+    result = (temperature * 9/5) + 32
+    print(f"{temperature} Celsius is {result} Fahrenheit")
+elif X == "2":
+    result = (temperature - 32) * 5/9
+    print(f"{temperature} Fahrenheit is {result} Celsius")
+else:
+    print("Invalid conversion type")
