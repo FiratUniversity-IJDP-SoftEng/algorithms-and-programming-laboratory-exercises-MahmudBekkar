1f1283ae4df830eee09a4e8fd99959caa657673b
MahmudBekkar <mahmud.bekkar99@gmail.com>
2025-03-16 15:56:03 +0300

    Update ex010.py

--- Exercises/ex010.py
+++ Exercises/ex010.py
@@ -1,2 +1,18 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 220543602
+# Your Name and Surname: Mahmud Bekkar
+x = int(input("Enter a number: "))
+y = int(input("Enter a number: "))
+operation = input("INPUT (+, -, *, /) : ")
+if operation == "+":
+    SUM = x + y
+elif operation == "-":
+    SUM = x - y
+elif operation == "*":
+    SUM = x * y
+else:
+    if y == 0:
+        SUM = ""
+        print("Error! Division by Zero")
+    else:
+        SUM = x / y
+print(SUM)
