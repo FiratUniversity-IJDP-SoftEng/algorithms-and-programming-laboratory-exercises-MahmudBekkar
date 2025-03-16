6e826c0a797731228afaf24d6e50f62bea07673f
MahmudBekkar <mahmud.bekkar99@gmail.com>
2025-03-16 15:20:00 +0300

    Update ex007.py

--- Exercises/ex007.py
+++ Exercises/ex007.py
@@ -1,2 +1,9 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 220543602
+# Your Name and Surname: Mahmud Bekkar
+user = input("Enter a string : ")
+y = {}
+for i in user:
+    y[i] = y.get(i, 0) + 1
+print("Character frequencies :")
+for i in sorted(y):
+    print(f"{i}: {y[i]}")
